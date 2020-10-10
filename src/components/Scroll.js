526266294914f02d4770ceb830f0030f801a7b1a
Sindu Andita <sinduandita@gmail.com>
2020-10-11 04:14:11 +0700

    Final Product + File Structuring + Refactoring

---
+++ src/components/Scroll.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Scroll = (props) => {
+  return (
+    <div
+      style={{
+        overflowX: "scroll",
+        border: "1px solid black",
+        height: "800px",
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default Scroll;
