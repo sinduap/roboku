923626176f02739014f1b11ac5d9f25b7f40e853
Sindu Andita <sinduandita@gmail.com>
2020-10-12 10:56:12 +0700

    Updates

---
+++ precache-manifest.db24cdc7806cc2811cb69858b663de37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ec1e2ebc581305f58b58737ae7e436",
+    "url": "/index.html"
+  },
+  {
+    "revision": "021a034d2ea06a242e5f",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f089fbcb3145a8320c39",
+    "url": "/static/css/main.09a91277.chunk.css"
+  },
+  {
+    "revision": "021a034d2ea06a242e5f",
+    "url": "/static/js/2.1b9bb12d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1b9bb12d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f089fbcb3145a8320c39",
+    "url": "/static/js/main.e9b1b9ed.chunk.js"
+  },
+  {
+    "revision": "2f4688434c8daa867ad8",
+    "url": "/static/js/runtime-main.45526f23.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
